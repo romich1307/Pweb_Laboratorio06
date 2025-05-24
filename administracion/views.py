fa1aa43b770c203b6b85b5ab7e95fa6a5a19114d
Paola0601 <pmaytaqu@unsa.edu.pe>
2025-05-24 18:22:16 -0500

    Implementamos el archivo views.py para las vistas d el formulario

--- administracion/views.py
+++ administracion/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import AlumnoForm, CursoForm, NotaAlumnoCursoForm
 
-# Create your views here.
+def agregar_alumno(request):
+    if request.method == 'POST':
+        form = AlumnoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('agregar_alumno')
+    else:
+        form = AlumnoForm()
+    return render(request, 'agregar_alumno.html', {'form': form})
+
+def agregar_curso(request):
+    if request.method == 'POST':
+        form = CursoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('agregar_curso')
+    else:
+        form = CursoForm()
+    return render(request, 'agregar_curso.html', {'form': form})
+
+def agregar_nota(request):
+    if request.method == 'POST':
+        form = NotaAlumnoCursoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('agregar_nota')
+    else:
+        form = NotaAlumnoCursoForm()
+    return render(request, 'agregar_nota.html', {'form': form})

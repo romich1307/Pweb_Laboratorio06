834a850728edadb55053657b69759727277ea3e3
romich1307 <rcamargoh@unsa.edu.pe>
2025-05-24 18:12:55 -0500

    admin: registrado modelo Curso  con columnas visibles, filtros y búsqueda en el panel

--- administracion/admin.py
+++ administracion/admin.py
@@ -6,3 +6,8 @@ class AlumnoAdmin(admin.ModelAdmin):
     list_display = ('id', 'nombre', 'apellido', 'dni', 'correo', 'numeroDeSemestre')
     search_fields = ('nombre', 'apellido', 'dni')
     list_filter = ('numeroDeSemestre',)
+
+@admin.register(Curso)
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'codigo')
+    search_fields = ('nombre', 'codigo')

cfe2a0e39b4382f3a0ff618cb0e0b70fd0401fe4
Paola0601 <pmaytaqu@unsa.edu.pe>
2025-05-24 18:02:55 -0500

    Realizamos el modelo de Alumno

--- administracion/models.py
+++ administracion/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class Alumno(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    dni = models.CharField(max_length=8, unique=True)
+    correo = models.EmailField(unique=True)
+    numeroDeSemestre = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
 # Create your models here.

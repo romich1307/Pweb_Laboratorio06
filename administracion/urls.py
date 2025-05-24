320bab39aa23f9b4b99fe492c7223111ce25064c
Paola0601 <pmaytaqu@unsa.edu.pe>
2025-05-24 18:23:35 -0500

    Creamos un archivo para las urls

---
+++ administracion/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('agregar_alumno/', views.agregar_alumno, name='agregar_alumno'),
+    path('agregar_curso/', views.agregar_curso, name='agregar_curso'),
+    path('agregar_nota/', views.agregar_nota, name='agregar_nota'),
+]
